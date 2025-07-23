91318804bb53ba7a42ad789b904b9e763159226f
Frish2021 <1573880184@qq.com>
2025-07-23 12:28:00 +0800

    更新构建脚本

--- scripts/set_env.sh
+++ scripts/set_env.sh
@@ -2,8 +2,11 @@ prop() {
   grep "^[[:space:]]*${1}" gradle.properties | cut -d'=' -f2 | sed 's/^[[:space:]]*//; s/\r//'
 }
 
+commit_tag=$(git log --pretty='%h' -1)
+
 # Set environment variable
-echo "commit_tag=$(git log --pretty='%h' -1)" >> $GITHUB_ENV
+echoi "release_tag=$(prop mcVersion)-${commit_tag}" >> $GITHUB_ENV
+echo "commit_tag=${commit_tag}" >> $GITHUB_ENV
 echo "commit_msg=$(git log --pretty='> [%h] %s' -1)" >> $GITHUB_ENV
 echo "branch_name=$(git branch --show-current)" >> $GITHUB_ENV
 echo "mcversion=$(prop mcVersion)" >> $GITHUB_ENV
