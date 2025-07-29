10b4b04b6db91b5674f5888d09dd575b0b66bde3
Frish2021 <1573880184@qq.com>
2025-07-30 00:15:42 +0800

    Update config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vitepress'
 
 export default defineConfig({
-    base: '/lemint',
+    base: '/Lemint',
     titleTemplate: 'LemonMint | :title',
     themeConfig: {
         siteTitle: 'LemonMint',
