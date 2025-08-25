d44dfdd5a2165a34834378c3f880c792d2db1f8b
Frish2021 <1573880184@qq.com>
2025-08-25 16:58:23 +0800

    重构LemonMint

--- lemonmint-script/build.gradle.kts
+++ lemonmint-script/build.gradle.kts
@@ -1,5 +1,5 @@
 dependencies {
-    implementation(project(":lemint-api"))
+    implementation(project(":lemonmint-api"))
     implementation("org.graalvm.polyglot:polyglot:24.2.2")
     implementation("org.graalvm.polyglot:js:24.2.2")
     testImplementation("org.junit.jupiter:junit-jupiter:5.12.2")
