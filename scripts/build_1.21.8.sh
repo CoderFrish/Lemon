fbb1666eea18bf265ee95151861680e0cb38b3d0
Frish2021 <1573880184@qq.com>
2025-09-30 21:51:52 +0800

    Added GitHub Build Stream CI.

---
+++ scripts/build_1.21.8.sh
@@ -0,0 +1,23 @@
+prop() {
+  grep "^[[:space:]]*${1}" gradle.properties | cut -d'=' -f2 | sed 's/^[[:space:]]*//; s/\r//'
+}
+
+mcversion=$(prop mcVersion)
+grdversion=$(prop version)
+commitid=$(git log --pretty='%h' -1)
+build_date=$(date -u +'%Y-%m-%d %H:%M:%S UTC')
+
+git config --global user.email "no-reply@github.com"
+git config --global user.name "Github Actions"
+chmod +x gradlew
+
+./gradlew applyAllPatches
+./gradlew createMojmapPaperclipJar
+
+echo "version=$grdversion" >> $GITHUB_ENV
+echo "mcversion=$mcversion" >> $GITHUB_ENV
+echo "commit_id=$commitid" >> $GITHUB_ENV
+echo "build_date=$BUILD_DATE" >> $GITHUB_ENV
+echo "commit_msg=$(git log --pretty='> [%h] %s' -1)" >> $GITHUB_ENV
+
+mv traium-server/build/libs/traium-paperclip-$grdversion-mojmap.jar build/libs/traium-$grdversion.jar
