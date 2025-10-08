82816b86c973298f9e6fa87fe1fa29aa160d3fb1
Frish2021 <1573880184@qq.com>
2025-10-08 13:31:49 +0800

    Fixed legacy bukkit plugin deprecated api.

---
+++ traium-server/src/main/java/me/coderfrish/traium/unsafe/UnsafeWorldData.java
@@ -0,0 +1,22 @@
+package me.coderfrish.traium.unsafe;
+
+import io.papermc.paper.threadedregions.RegionizedWorldData;
+import net.minecraft.world.level.Level;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class UnsafeWorldData {
+    private static final Map<Level, RegionizedWorldData> unsafeWorldData = new ConcurrentHashMap<>();
+
+    public static RegionizedWorldData getUnsafeWorldData(Level world) {
+        if (!unsafeWorldData.containsKey(world))
+            return null;
+
+        return unsafeWorldData.get(world);
+    }
+
+    public static void addUnsafeWorldData(RegionizedWorldData world) {
+        unsafeWorldData.put(world.world, world);
+    }
+}
