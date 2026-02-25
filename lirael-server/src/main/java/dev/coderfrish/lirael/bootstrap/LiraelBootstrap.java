44ecfc2dbb505248b67197c020d3561009ce9b6d
CoderFrish <1573880184@qq.com>
2026-02-25 22:18:09 +0800

    (feat.) 添加了对jei的原生支持，让服务端能主动给装有jei模组的客户端发送配方

--- lirael-server/src/main/java/dev/coderfrish/lirael/bootstrap/LiraelBootstrap.java
+++ lirael-server/src/main/java/dev/coderfrish/lirael/bootstrap/LiraelBootstrap.java
@@ -1,9 +1,26 @@
 package dev.coderfrish.lirael.bootstrap;
 
 import dev.coderfrish.lirael.config.LiraelConfig;
+import dev.coderfrish.lirael.listener.RecipeSyncListener;
+import dev.coderfrish.lirael.utility.FakePlugin;
+import org.bukkit.Bukkit;
+import org.bukkit.Server;
+import org.bukkit.plugin.messaging.Messenger;
 
 public class LiraelBootstrap {
+    private static final FakePlugin fakePlugin = new FakePlugin();
+
     public static void bootstrap() throws Exception {
         LiraelConfig.load(); /* lirael config */
+    }
+
+    public static void startup() {
+        final Server server = Bukkit.getServer();
+        final Messenger messenger = server.getMessenger();
+
+        server.getPluginManager().registerEvents(new RecipeSyncListener(), fakePlugin);
+
+        messenger.registerOutgoingPluginChannel(fakePlugin, "neoforge:recipe_content");
+        messenger.registerOutgoingPluginChannel(fakePlugin, "fabric:recipe_sync");
     }
 }
